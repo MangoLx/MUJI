af98dbbb0a01339f171f5311df9cdc8521416ffc
MangoLx <18381060672@163.com>
2019-01-10 10:21:34 +0800

    feat：添加列表页排序功能（价格降序，其他乱序）

--- src/js/allFood.js
+++ src/js/allFood.js
@@ -6,6 +6,7 @@ require(['./requirejs.config'], () => {
             listItem.init();
             waterFull()
             toDetail();
+            sort();
         })()
 
         // 滚动刷新商品
@@ -21,11 +22,48 @@ require(['./requirejs.config'], () => {
                 }
             })
         }
+        // 点击商品去详情页
         function toDetail() {  
             // 事件委托
             $('#list-item').on('click', 'li', function () { 
                 location.href = '/html/detail.html?id=' + $(this).data('id');
             });
+        }
+        // 点击排序进行排序
+        function sort() {  
+            $('.note-choose').on('click', 'span', function () {  
+                // 添加当前状态class
+                $(this).addClass('cur').siblings().removeClass('cur');
+                // 按照价格排序
+                if($(this).prop('id') === 'sort_price'){
+                    // 获取所有盒子
+                    let aLi = Array.from($('#list-item').find('li'));
+                    let nowALi =  aLi.sort(function (first, second) {  
+                        return Number($(second).find('span').text().slice(1)) - 
+                                Number($(first).find('span').text().slice(1));
+                    })
+                    // 将得到的排序结果更新到数据
+                    // 清空原来的html
+                    $('#list-item').html('');
+                    $(nowALi).each(function (index, item) {  
+                        $(item).appendTo($('#list-item'));
+                    })
+                }
+                // 乱序
+                else{
+                    // 获取所有盒子
+                    let aLi = Array.from($('#list-item').find('li'));
+                    let nowALi =  aLi.sort(function (first, second) {  
+                        return Math.random()>.5 ? -1 : 1;
+                    })
+                    // 将得到的排序结果更新到数据
+                    // 清空原来的html
+                    $('#list-item').html('');
+                    $(nowALi).each(function (index, item) {  
+                        $(item).appendTo($('#list-item'));
+                    })
+                }
+            })
         }
     })
 })
